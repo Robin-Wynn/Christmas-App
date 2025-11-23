2147f28a61b148888efabdd5e3bdc5913fb4b267
Robin-Wynn <wynnrobin95@gmail.com>
2025-11-22 18:40:55 -0600

    complete entering data and creating tables 💪🏼

--- database/schema.sql
+++ database/schema.sql
@@ -50,6 +50,28 @@ CREATE TABLE program(
     CONSTRAINT fk_producer FOREIGN KEY (producer_id) REFERENCES producer (producer_id)
 );
 
+-- pivot tables
+CREATE TABLE program_to_director(
+    program_id MEDIUMINT UNSIGNED NOT NULL,
+    director_id SMALLINT UNSIGNED NOT NULL,
+    CONSTRAINT fk_prog_dir FOREIGN KEY (program_id) REFERENCES program (program_id),
+    CONSTRAINT fk_dir_prog FOREIGN KEY (director_id) REFERENCES director (director_id)
+);
+
+CREATE TABLE program_to_streaming(
+    program_id MEDIUMINT UNSIGNED NOT NULL,
+    streaming_platform_id SMALLINT UNSIGNED NOT NULL,
+    CONSTRAINT fk_prog_str FOREIGN KEY (program_id) REFERENCES program (program_id),
+    CONSTRAINT fk_str_prog FOREIGN KEY (streaming_platform_id) REFERENCES streaming_platform (streaming_platform_id)
+);
+
+CREATE TABLE program_to_actor(
+    program_id MEDIUMINT UNSIGNED NOT NULL,
+    actor_id MEDIUMINT UNSIGNED NOT NULL,
+    CONSTRAINT fk_prog_act FOREIGN KEY (program_id) REFERENCES program (program_id),
+    CONSTRAINT fk_act_prog FOREIGN KEY (actor_id) REFERENCES actor (actor_id)
+);
+
 -- ALTERATIONS
 ALTER TABLE actor 
 MODIFY actor_id MEDIUMINT UNSIGNED AUTO_INCREMENT NOT NULL;
@@ -57,5 +79,13 @@ MODIFY actor_id MEDIUMINT UNSIGNED AUTO_INCREMENT NOT NULL;
 ALTER TABLE program 
 MODIFY robins_rating ENUM('NA', '1', '2', '3', '4', '5') DEFAULT '5';
 
-ALTER TABLE program 
-MODIFY runtime SMALLINT UNSIGNED NOT NULL,
+ALTER TABLE program
+MODIFY fun_fact TEXT;
+
+ALTER TABLE program
+MODIFY img_url VARCHAR(60);
+
+-- reset auto increment program table
+SET @num := 0;
+UPDATE program SET program_id = @num := (@num + 1);
+ALTER TABLE program AUTO_INCREMENT = 1;
